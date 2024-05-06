982184d5ce293d92496cf8ed0d420f1e95846334
sebasmpvofficial <166235155+sebasmpvofficial@users.noreply.github.com>
2024-05-06 18:28:35 -0500

    Create Instagrambot.js

---
+++ plugins/Instagrambot.js
@@ -0,0 +1,17 @@
+import fg from 'api-dylux' 
+import fetch from 'node-fetch'
+import { savefrom, facebookdl, facebookdlv2 } from '@bochilteam/scraper'
+import fbDownloader from 'fb-downloader-scrapper'
+import { facebook } from "@xct007/frieren-scraper"
+import axios from 'axios'
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+if (!args[0]) throw `
+https://www.instagram.com/invites/contact/?i=1guo4p9rge3t8&utm_content=u61pkme
+                
+` 
+}
+handler.command = /^(instagrambot|igbot)$/i
+handler.register = false
+handler.group = false
+handler.admin = false
+export default handler
