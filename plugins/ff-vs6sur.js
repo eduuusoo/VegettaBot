65af1a2cfa938675c9852312c329d9b4f3f8440e
sebasmpvofficial <166235155+sebasmpvofficial@users.noreply.github.com>
2024-05-06 17:58:50 -0500

    Create ff-vs6sur.js

---
+++ plugins/ff-vs6sur.js
@@ -0,0 +1,39 @@
+import fg from 'api-dylux' 
+import fetch from 'node-fetch'
+import { savefrom, facebookdl, facebookdlv2 } from '@bochilteam/scraper'
+import fbDownloader from 'fb-downloader-scrapper'
+import { facebook } from "@xct007/frieren-scraper"
+import axios from 'axios'
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+if (!args[0]) throw `
+╭──────⚔──────╮
+ㅤㅤㅤ6 𝐕𝐄𝐑𝐒𝐔𝐒 6
+╰──────⚔──────╯
+╭──────────────╮
+│ㅤ⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎 
+│ㅤ🇦🇷 𝐀𝐑𝐆 : 
+│ㅤ🇵🇪 𝐏𝐄𝐑 : 
+│➥ 𝐌𝐎𝐃𝐀𝐋𝐈𝐃𝐀𝐃: 
+│➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
+│
+│     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+│⚜️ ➤ 
+│⚜️ ➤ 
+╰─────────────╯
+          
+` 
+}
+handler.command = /^(vs6sur|6vs6sur)$/i
+handler.register = true
+handler.group = true
+handler.admin = true
+export default handler
