213e36ffa3fd428bbdf2ce340953d786e7934ff1
sebasmpvofficial <166235155+sebasmpvofficial@users.noreply.github.com>
2024-05-06 17:51:59 -0500

    Create fun-readmore.js

---
+++ plugins/fun-readmore.js
@@ -0,0 +1,15 @@
+
+const handler = async (m, {conn, text}) => {
+    let [l, r] = text.split`|`;
+    if (!l) l = '';
+    if (!r) r = '';
+    conn.reply(m.chat, l + readMore + r, m);
+  };
+  handler.help = ['readmore', 'spoiler'].map((v) => v + ' <teks>|<teks>');
+  handler.tags = ['tools'];
+  handler.command = /^(hidetext|readmore|leermas)$/i;
+  export default handler;
+  
+  const more = String.fromCharCode(8206);
+  const readMore = more.repeat(4001);
+  
