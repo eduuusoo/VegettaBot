c1eba4957801a55c6190a6e0e21af33a257437b3
sebasmpvofficial <166235155+sebasmpvofficial@users.noreply.github.com>
2024-05-06 17:57:42 -0500

    Create ff-vs8.js

---
+++ plugins/ff-vs8.js
@@ -0,0 +1,45 @@
+
+import fg from 'api-dylux' 
+import fetch from 'node-fetch'
+import { savefrom, facebookdl, facebookdlv2 } from '@bochilteam/scraper'
+import fbDownloader from 'fb-downloader-scrapper'
+import { facebook } from "@xct007/frieren-scraper"
+import axios from 'axios'
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+if (!args[0]) throw `
+╭──────⚔──────╮
+ㅤㅤㅤ8 𝐕𝐄𝐑𝐒𝐔𝐒 8
+╰──────⚔──────╯
+╭──────────────╮
+│ㅤ⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎  
+│ㅤ🇲🇽 𝐌𝐄𝐗 : 
+│ㅤ🇨🇴 𝐂𝐎𝐋 : 
+│➥ 𝐂𝐎𝐋𝐎𝐑 𝐃𝐄 𝐑𝐎𝐏𝐀: 
+│➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
+│
+│     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│     
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 2
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+│⚜️ ➤ 
+│⚜️ ➤ 
+╰─────────────╯
+               
+` 
+}
+handler.command = /^(vs8|8vs8)$/i
+handler.register = true
+handler.group = true
+handler.admin = true
+export default handler
