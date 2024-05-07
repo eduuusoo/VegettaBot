78ddeae452f2454026f6c0dd763856432f0206ab
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-05-07 06:02:53 -0500

    Actualizar grupos-admin.js

--- plugins/grupos-admin.js
+++ plugins/grupos-admin.js
@@ -171,7 +171,7 @@ let oi = `» 🔱 ${lenguajeGB['smsAddB5']()} ${pesan}`
 let teks = `${lenguajeGB['smstagaa']()}\n\n${oi}\n\n`
 for (let mem of participants) {
 teks += `➥🔱@${mem.id.split('@')[0]}\n`}
-teks += `*AQUAMAN BOT¹*`
+teks += `*𝙑𝙀𝙂𝙀𝙏𝙏𝘼 𝘽𝙊𝙏*`
 await conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )    
 break
     
