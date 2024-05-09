975b7b9f157c44c71a7c851a4cf1222c3b0000ad
sebasmpvofficial <166235155+sebasmpvofficial@users.noreply.github.com>
2024-05-08 20:15:08 -0500

    Update menuimg.js

--- plugins/menuimg.js
+++ plugins/menuimg.js
@@ -407,7 +407,7 @@ await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() 
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(e)}}
 
-handler.command = /^(menu|menú|help|allmenu|\?)$/i
+handler.command = /^(menu|menú|help|allmenu|menuu|almenu|almenuu|\?)$/i
 //handler.register = true
 export default handler
 
